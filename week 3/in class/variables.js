2c072ebeacfcf57d1114e78828088a0f5a355e3e
yharit <66433115+yharit@users.noreply.github.com>
2025-10-03 11:16:29 -0700

    migrating

---
+++ week 3/in class/variables.js
@@ -0,0 +1,29 @@
+//var name = "yajurvi";
+//let location = "los angeles";
+//const weather = 'sunny';
+//let year = 2025;
+//let happy = true;
+//let person = {}
+//person.name = name;
+//console.log(person);
+
+//end of 1
+
+// function numberAdder(firstNumber, secondNumber) {
+// 	let sum;
+// 	sum = firstNumber + secondNumber;
+// 	return sum;
+// }
+// console.log(numberAdder(10,5));
+
+//end of 2
+
+console.log("hello");
+
+let Things = ['1', '2', '3']
+for (var i = 0; i < Things.length; i++) {
+console.log(Things[i])
+}
+
+
+// if (Things)
